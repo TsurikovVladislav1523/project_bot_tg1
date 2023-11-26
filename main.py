58f26c59afdab6cf18d62213d8bec407d8dd697b
Цуриков Владислав <tsurikovvladisalv@yandex.ru>
2023-11-26 22:13:10 +0300

    26.11

--- main.py
+++ main.py
@@ -13,17 +13,18 @@ from texts import *
 from add_token import add_token
 from db import *
 
-
 TOKEN_ID = "6497123928:AAE_TTI7TKoI9iB1W1Ys_1NuEJtKs9wSQSk"
 bot = Bot(token=TOKEN_ID)
 dp = Dispatcher(bot, storage=MemoryStorage())
 CLOUD_TOKEN = '8e34e01b502a43c68fb0bb9ba2956df5'
 URL = 'https://cloud-api.yandex.net/v1/disk/resources'
 TOKEN = ''
+all_headers = {}
 headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {TOKEN}'}
 
 URI_INFO = f'https://api.telegram.org/bot{TOKEN_ID}/getFile?file_id=AgACAgIAAxkBAANYZSBoUZeuyA1XuR4JfzVLgbWQ4mQAAvnPMRsQEgABSbK7DYRZJAwBAQADAgADbQADMAQ'
 URI = f'https://api.telegram.org/file/bot{TOKEN_ID}/'
+
 
 # headers_users = {}  !!!!Тут поставить headers для одновременной работы с ботом нескольких пользователей!!!!
 
@@ -34,18 +35,22 @@ class Form(StatesGroup):
     code_fam = State()
 
 
+class LoadIm(StatesGroup):
+    path = State()
 
-def create_folder(path):
+
+def create_folder(path, tg_id):
     """Создание папки"""
-    requests.put(f'{URL}?path=photos/{path}', headers=headers)
+    requests.put(f'{URL}?path=photos/{path}', headers=all_headers[tg_id])
 
 
-def upload_file(path, name, replace=False):
+def upload_file(path, tg_id, name, replace=False):
     """Загрузка файла.
     name: Название фото
     path: Папка на диске
     replace: true or false Замена файла на Диске"""
-    res = requests.get(f'{URL}/upload?path=photos/{path}/{name}.png&overwrite={replace}', headers=headers).json()
+    res = requests.get(f'{URL}/upload?path=photos/{path}/{name}.png&overwrite={replace}',
+                       headers=all_headers[tg_id]).json()
     with open(f'photos/{name}.png', 'rb') as f:
         try:
             requests.put(res['href'], files={'file': f})
@@ -60,7 +65,8 @@ async def start(message: types.Message):
     tg_id = message.from_user.id
     if get_token_by_tg_id(tg_id):
         TOKEN = get_token_by_tg_id(tg_id)
-        headers["Authorization"] = f'OAuth {TOKEN}'
+        all_headers[tg_id] = headers
+        all_headers[tg_id]["Authorization"] = f'OAuth {TOKEN}'
         update_emb(TOKEN)
         await message.answer(TOKEN)
     else:
@@ -82,7 +88,9 @@ async def start(message: types.Message):
                 global headers
                 code = message.text
                 TOKEN = add_token(code)
-                headers["Authorization"] = f'OAuth {TOKEN}'
+                tg_id = message.from_user.id
+                all_headers[tg_id] = headers
+                all_headers[tg_id]["Authorization"] = f'OAuth {TOKEN}'
                 await message.answer(TOKEN)
                 key = ''
                 for i in range(16):
@@ -92,11 +100,9 @@ async def start(message: types.Message):
                 await message.answer(key)
                 tg_id = message.from_user.id
                 add_main_family_user(tg_id, TOKEN, key)
-                requests.put(f'{URL}?path=photos', headers=headers)
+                requests.put(f'{URL}?path=photos', headers=all_headers[tg_id])
                 await message.answer('Успешно')
                 await state.finish()
-
-
 
         @dp.message_handler(Text(equals="Вход в семейный аккаунт"))
         async def fam(message: types.Message):
@@ -111,7 +117,8 @@ async def start(message: types.Message):
             secret_key = message.text
             add_other_family_user(tg_id, secret_key)
             TOKEN = get_token_by_tg_id(tg_id)
-            headers["Authorization"] = f'OAuth {TOKEN}'
+            all_headers[tg_id] = headers
+            all_headers[tg_id]["Authorization"] = f'OAuth {TOKEN}'
             await message.answer('Успешно')
             await state.finish()
 
@@ -127,60 +134,89 @@ async def start(message: types.Message):
             global headers
             code = message.text
             TOKEN = add_token(code)
-            headers["Authorization"] = f'OAuth {TOKEN}'
+            tg_id = message.from_user.id
+            all_headers[tg_id] = headers
+            all_headers[tg_id]["Authorization"] = f'OAuth {TOKEN}'
             '''' добвавить бд'''
             tg_id = message.from_user.id
             add_solo_user(tg_id, TOKEN)
-            requests.put(f'{URL}?path=photos', headers=headers)
+            requests.put(f'{URL}?path=photos', headers=all_headers[tg_id])
             update_emb(TOKEN)
             await message.answer('Успешно')
             await state.finish()
 
 
-@dp.message_handler(commands=['load_image'])
+@dp.message_handler(commands=['sort_image'])
 async def load(message: types.Message):
     global path_flag
     '''было бы непохо потом переписать на state'''
     path_flag = False
-    await message.answer("Введите название фото")
+    await message.answer("Отправьте фото")
 
     @dp.message_handler(content_types=['text'])
     async def name(message: types.Message):
         global name
         if path_flag:
             path = message.text
-            create_folder(path)
-            upload_file(path, name)
+            tg_id = message.from_user.id
+            create_folder(path, tg_id)
+            upload_file(path, tg_id, name)
             os.remove(f'photos/{name}.png')
             update_emb(TOKEN)
             await message.answer(f'Сохранение фото в папку "{path}"')
+
+    @dp.message_handler(content_types=['photo'])
+    async def process_photo(message: types.Message):
+        global name, path_flag
+        file_id = message.photo[-1]["file_id"]
+        URI_INFO = f'https://api.telegram.org/bot{TOKEN_ID}/getFile?file_id=' + file_id
+        resp = requests.get(URI_INFO)
+        img_path = resp.json()['result']['file_path']
+        img = requests.get(URI + img_path)
+        img = Image.open(io.BytesIO(img.content))
+        tg_id = message.from_user.id
+        name = str(tg_id) + str(random.randint(0, 1000000))
+        print(name)
+        img.save(f'photos/{name}.png', format="PNG")
+        img.close()
+        tg_id = message.from_user.id
+        pathes = search(f'photos/{name}.png')
+        print(pathes)
+        if pathes:
+            for path in pathes:
+                upload_file(path, tg_id, name)
+            os.remove(f'photos/{name}.png')
+            await message.answer(f'Сохранение фото в папку "{path}"')
         else:
-            name = message.text
-            await message.answer("Отправьте фото")
+            await message.answer(
+                'Данный человек не найден в базе данных, пожалуйста, введите название папки для этого человека на латинице')
+            path_flag = True
+
+
+@dp.message_handler(commands=['load_image'])
+async def load_w_s(message: types.Message):
+    await message.answer(WITOUT_AI)
+
+    @dp.message_handler(content_types=['text'])
+    async def name_path(message: types.Message):
+        path = message.text
 
         @dp.message_handler(content_types=['photo'])
-        async def process_photo(message: types.Message):
-            global name, path_flag
+        async def name_path(message: types.Message):
             file_id = message.photo[-1]["file_id"]
-
+            tg_id = message.from_user.id
             URI_INFO = f'https://api.telegram.org/bot{TOKEN_ID}/getFile?file_id=' + file_id
             resp = requests.get(URI_INFO)
             img_path = resp.json()['result']['file_path']
             img = requests.get(URI + img_path)
-            img = Image.open(io.BytesIO(img.content))
-            img.save(f'photos/{name}.png', format="PNG")
-            img.close()
-            pathes = search(f'photos/{name}.png')
-            if pathes:
-                for path in pathes:
-                    upload_file(path, name)
-                os.remove(f'photos/{name}.png')
-                await message.answer(f'Сохранение фото в папку "{path}"')
-            else:
-                await message.answer(
-                    'Данный человек не найден в базе данных, пожалуйста, введите название папки для этого человека на латинице')
-                path_flag = True
-
+            res = requests.get(f'{URL}/upload?path={path}/{tg_id}{random.randint(0, 1000000)}.png&overwrite=False',
+                               headers=all_headers[tg_id]).json()
+            print(res.keys())
+            if "error" in res.keys():
+                requests.put(f'{URL}?path={path}', headers=all_headers[tg_id])
+                res = requests.get(f'{URL}/upload?path={path}/{tg_id}{random.randint(0, 1000000)}.png&overwrite=False',
+                                   headers=all_headers[tg_id]).json()
+            requests.put(res['href'], files={'file': io.BytesIO(img.content)})
 
 
 if __name__ == "__main__":
